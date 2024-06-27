f635e4ee34f70ac35c0090f62fd2bff5f8e2949f
RiccardoMacchi <rick.mac97@outlook.com>
2024-06-27 10:02:26 +0200

    aggiunto v-show per display messaggio d'errore aggiunto enter per aggiunta todo baypassato problema rimozione classe a todo sottostante a quello eliminato

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
+        show : false,
         logoImg : "img/boolean.png",
         newItem : {
             text : "",
@@ -19,11 +20,18 @@ createApp({
     methods: {
         removeTodo(i){
             this.tasks.splice(i,1)
+            this.toggleDone(i)
         },
         addTask(){
-            console.log(this.newItem)
-            this.tasks.unshift({...this.newItem});
-            // this.newItem.text = ""
+            console.log(this.newItem.text.length)
+            if(this.newItem.text.length < 5){
+                this.show = true;
+            } else {
+                this.tasks.unshift({...this.newItem});
+                this.show = false;
+                this.newItem.text = ""
+            }
+            
         },
         toggleDone(i){
             console.log("hai cliccato")
